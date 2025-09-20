63f6cffdb1a4dfe514e0e11f421b372fc0a3a862
Yershek <homohojel3@gmail.com>
2025-09-20 09:03:06 +0600

    project create

---
+++ src/main/java/kg/laponandsweezy/customs_management_system/CustomsManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package kg.laponandsweezy.customs_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomsManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomsManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/laponandsweezy/customs_management_system/CustomsManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.laponandsweezy.customs_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomsManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
